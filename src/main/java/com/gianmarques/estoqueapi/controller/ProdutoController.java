161602500679692798c53cb72f0535eb6b673167
Giancarlo Marques <gian_marques001@outlook.com>
2025-03-01 23:24:29 -0300

    Implementando Produto 1.1

--- src/main/java/com/gianmarques/estoqueapi/controller/ProdutoController.java
+++ src/main/java/com/gianmarques/estoqueapi/controller/ProdutoController.java
@@ -5,12 +5,14 @@ import com.gianmarques.estoqueapi.dto.ProdutoResponseDto;
 import com.gianmarques.estoqueapi.entity.Produto;
 import com.gianmarques.estoqueapi.entity.enums.ECategoriaProduto;
 import com.gianmarques.estoqueapi.service.IProdutoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/produtos")
@@ -22,9 +24,13 @@ public class ProdutoController {
         this.produtoService = produtoService;
     }
 
+    @GetMapping
+    public List<Produto> listar() {
+        return produtoService.listar();
+    }
 
     @PostMapping
-    public ResponseEntity<ProdutoResponseDto> salvarProduto(@RequestBody ProdutoRequestDto produtoRequestDto) {
+    public ResponseEntity<ProdutoResponseDto> salvar(@RequestBody ProdutoRequestDto produtoRequestDto) {
         System.out.println("Categoria " + produtoRequestDto.categoria());
         System.out.println("Nome " + produtoRequestDto.nome());
         Produto produto = new Produto();
@@ -45,8 +51,27 @@ public class ProdutoController {
         return ResponseEntity.created(url).body(responseDto);
     }
 
-    @GetMapping
-    public List<Produto> listarProdutos(){
-        return produtoService.listar();
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+        Optional<Produto> produto = produtoService.buscarPorId(id);
+        if (produto.isPresent()) {
+            return ResponseEntity.ok(produto.get());
+        }
+        return ResponseEntity.noContent().build();
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long id) {
+        produtoService.deletarPorId(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Produto> atualizarPorId(@PathVariable Long id, @RequestBody Produto produtoRequestDto) {
+       Produto produtoAtualizado =  produtoService.editarPorId(id, produtoRequestDto);
+
+        return ResponseEntity.ok().body(produtoAtualizado);
+    }
+
 }
